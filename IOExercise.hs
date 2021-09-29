4ee39e06959d487e9ed41959152a01a301f9ebed
Sherry Shi <sherryshi2001@gmail.com>
2021-09-29 12:45:22 -0400

    complete IO exercises

--- IOExercise.hs
+++ IOExercise.hs
@@ -22,11 +22,25 @@ simpleProgram = do
   inpStr <- getLine
   putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!"
 
+simpleProgram' :: IO ()
+simpleProgram' =
+  putStrLn "This is a simple program that does IO"
+    >> putStrLn "What is your name?"
+    >> getLine >>= (\inpStr -> putStrLn $ "Welcome to Haskell, " ++ inpStr ++ "!")
+
 lengthProgram :: IO Int
 lengthProgram = do
   let x = length [1, 2, 3, 4, 5, 6]
   putStrLn $ "The length of the list is" ++ show x
   return x
+
+lengthProgram' :: IO Int
+lengthProgram' = (\x -> putStrLn ("The length of the list is " ++ show x) >> return x) (length [1, 2, 3, 4, 5, 6])
+
+-- let x = length [1,2,3,4,5,6] in putStrLn $ "The length of the list is " ++ show x
+
+-- lengthProgram'' :: IO Int
+-- lengthProgram'' = return (length [1, 2, 3, 4, 5, 6]) >>= (\x -> (putStrLn $ "The length of the list is" ++ show x) >> return x)
 
 anotherProgram :: IO ()
 anotherProgram = do
@@ -38,3 +52,9 @@ anotherProgram = do
       return ()
     else putStrLn $ "Hello " ++ inpStr
   putStrLn "That's all!"
+
+anotherProgram' :: IO ()
+anotherProgram' =
+  putStrLn "What is your name?"
+    >> (getLine >>= \inpStr -> if inpStr == "Haskell" then putStrLn "you rock!" else putStrLn $ "Hello " ++ inpStr)
+    >> putStrLn "That's all!"
